9a3a770aeff577fd30d2f5ac3a4b41a2e224f554
jake <jake@jakecoffman.com>
2014-11-07 17:19:10 -0600

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#![feature(phase)]
+#[phase(plugin)]
+extern crate regex_macros;
+extern crate regex;
+
+// use regex::Regex;
+use std::rand;
+use std::rand::Rng;
+
 fn main() {
-    println!("Hello, world!")
+	// old and busted
+	// let re = match Regex::new(r"^\d+$") {
+	// 	Ok(re) => re,
+	// 	Err(err) => panic!("{}", err),
+	// };
+
+	// new hotness
+	let re = regex!(r"^\d+$");
+
+	match re.is_match("123") {
+		true => println!("That is correct"),
+		false => panic!("INSANE IN THE MEMBRANE")
+	}
+
+	match may_return_none() {
+		Some(val) => println!("{}", val),
+		None => println!("returned None"),
+	}
+
+	let thingy = (1i, true);
+	println!("{}", thingy)
+
+	let mut v = 0i;
+	println!("{}", v)
+	v = v - 1;
+	println!("{}", v)
 }
+
+fn may_return_none() -> Option<int> {
+	let mut rng = rand::task_rng();
+	let val = rng.gen::<int>(); 
+	if val % 2 == 0 {
+		return Some(val);
+	} else {
+		return None
+	}
+}
+
